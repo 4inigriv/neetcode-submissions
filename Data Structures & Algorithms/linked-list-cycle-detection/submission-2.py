6fc4a66651f363da733418d14d3ec2c841a92320
Virgínia <136639734+4inigriv@users.noreply.github.com>
2026-06-04 10:57:07 -0400

    Add: linked-list-cycle-detection - submission-2

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-2.py
@@ -0,0 +1,29 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+'''
+1-> passar por todos os nós
+2-> armazenar os nós
+3-> comparar com o no atual o nó armazenado
+4-> se ja passou por aquele nó, retorne verdadeiro 
+5-> se não, anda
+6-> se n td isso falso
+'''
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        nonow = head
+        armazen = set() #2
+        while nonow: #1
+            if nonow in armazen: #3
+                return True #4
+            else:
+                armazen.add(nonow) #2
+                nonow = nonow.next #5
+        return False #6
+'''
+TIME: O(n)
+SPACE: O(n)
+'''
+        
